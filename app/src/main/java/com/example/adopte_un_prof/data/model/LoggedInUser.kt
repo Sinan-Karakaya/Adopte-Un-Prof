d36c8b6d5b8d946290060dfb5368b8fef37fa87e
Sinan Karakaya <sinan.karakaya@epitech.eu>
2022-05-27 23:01:52 +0200

    feat: added login and register activities

---
+++ app/src/main/java/com/example/adopte_un_prof/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.adopte_un_prof.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
